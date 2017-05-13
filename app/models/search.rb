f5f5b247fcbfea7782df4bb5f012fc2abba82b2f
alankelly2011 <alankelly@email.com>
2017-05-13 16:20:24 +0000

    commented out eventtime

--- app/models/search.rb
+++ app/models/search.rb
@@ -8,7 +8,7 @@ class Search < ActiveRecord::Base
     events = events.where(["name LIKE ?", "%#{performer}%"]) if performer.present?
     events = events.where(["category LIKE ?", genre]) if genre.present?
     events = events.where(["location LIKE ?", place]) if place.present?
-    events = events.where(["eventtime >= ?", searchtime]) if searchtime.present?
+   # events = events.where(["eventtime >= ?", searchtime]) if searchtime.present?
     events = events.where(["date LIKE ?", date]) if date.present?
     events = events.where(["description LIKE ?", about]) if about.present?
     
