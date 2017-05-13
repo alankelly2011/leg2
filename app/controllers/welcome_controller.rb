27c498b8161ce830a980299937ebfd0ef418d7c8
alankelly2011 <alankelly@email.com>
2017-05-13 17:11:56 +0000

    put eventtime back in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,6 +5,6 @@ class WelcomeController < ApplicationController
     @performers = Event.uniq.pluck(:name)
     @genres = Event.uniq.pluck(:category)
     @places = Event.uniq.pluck(:location)
-  #  @searchtimes = Event.uniq.pluck(:eventtime)
+    @searchtimes = Event.uniq.pluck(:eventtime)
   end
 end
