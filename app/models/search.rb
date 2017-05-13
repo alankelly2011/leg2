72034f0e98cf9a4b2100f603d0a11223c1eb507e
alankelly2011 <alankelly@email.com>
2017-05-13 15:05:51 +0000

    added single quotes to search.rb instead of double

--- app/models/search.rb
+++ app/models/search.rb
@@ -5,12 +5,12 @@ class Search < ActiveRecord::Base
     
     events = Event.all
     
-    events = events.where(["\"name\" LIKE ?", "%#{performer}%"]) if performer.present?
-    events = events.where(["\"category\" LIKE ?", genre]) if genre.present?
-    events = events.where(["\"location\" LIKE ?", place]) if place.present?
-    events = events.where(["\"eventtime\" >= ?", searchtime]) if searchtime.present?
-    events = events.where(["\"date\" LIKE ?", date]) if date.present?
-    events = events.where(["\"description\" LIKE ?", about]) if about.present?
+    events = events.where(['name LIKE ?', '%#{performer}%']) if performer.present?
+    events = events.where(['category LIKE ?', genre]) if genre.present?
+    events = events.where(['location LIKE ?', place]) if place.present?
+    events = events.where(['eventtime >= ?', searchtime]) if searchtime.present?
+    events = events.where(['date LIKE ?', date]) if date.present?
+    events = events.where(['description LIKE ?', about]) if about.present?
     
     
     return events
