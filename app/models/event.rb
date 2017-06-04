d80f589ddf77b6b35cc0249c51b35466bf6e3fbd
alankelly2011 <alankelly@email.com>
2017-06-04 21:48:08 +0000

    change made to model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,18 +2,9 @@ class Event < ActiveRecord::Base
    
   validates :user_id, presence: true  
   
+  
   belongs_to :user
-  
-  config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_region => ENV['AWS_REGION'],
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
-}
-  
+
  has_attached_file :image
  
  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif", "application/pdf"],
