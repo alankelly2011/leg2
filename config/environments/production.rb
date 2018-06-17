9bbea1d63e3c59416be61496e567986a7e46f129
alankelly2011 <alankelly@email.com>
2018-06-17 14:25:53 +0000

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,10 +84,10 @@ config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                      587,
+    port:                      25,
     domain:                 'gmail.com',
-    user_name:           ENV["GMAIL_USERNAME"],
-    password:              ENV["GMAIL_PASSWORD"],
+    email_provider_username:  ENV["GMAIL_USERNAME"],
+    email_provider_password: ENV["GMAIL_PASSWORD"],
     authentication:      'plain',
     enable_starttls_auto: true  }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
